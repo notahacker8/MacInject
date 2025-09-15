5eeae21d1419cf7cb6f292d3f7ada1a40082eea4
King Doggo <77363964+notahacker8@users.noreply.github.com>
2025-09-15 16:19:30 -0700

    Update Mac-Inject-Arm64-Old.c

--- Mac-Inject-Arm64-Old.c
+++ Mac-Inject-Arm64-Old.c
@@ -191,9 +191,5 @@ int main(int argc, const char * argv[]) {
     kr(thread_create_running(task, ARM_THREAD_STATE64, (thread_state_t)&regs, ARM_THREAD_STATE64_COUNT, &remote_thread));
     sleep(1);
     kr(thread_terminate(remote_thread));
-    kr(vm_deallocate(task, remote_stack, STACK_SIZE));
-    kr(vm_deallocate(task, remote_mach_code, MACH_CODE_SIZE));
-    kr(vm_deallocate(task, remote_path, strlen(path)));
-    kr(vm_deallocate(task, remote_pthread_mem, 8));
     return 0;
 }
