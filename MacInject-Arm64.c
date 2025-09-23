cb97f8eb51d933327db7209b18cb878b71ca0c61
King Doggo <77363964+notahacker8@users.noreply.github.com>
2025-09-23 15:22:06 -0700

    Update MacInject-Arm64.c

--- MacInject-Arm64.c
+++ MacInject-Arm64.c
@@ -229,18 +229,7 @@ int main(int argc, const char * argv[]) {
     
     thread_act_t remote_thread;
     kr(thread_create_running(task, ARM_THREAD_STATE64, (thread_state_t)&regs, ARM_THREAD_STATE64_COUNT, &remote_thread));
-    
-    for (;;)
-    {
-        mach_msg_type_number_t sc = ARM_THREAD_STATE64_COUNT;
-        kr(thread_get_state(remote_thread, ARM_THREAD_STATE64, (thread_state_t)(&regs), &sc));
-        if (regs.__x[0] == 777)
-        {
-            printf("FINISHED INJECTING\n");
-            break;
-        }
-    }
-    
+    sleep(1);
     kr(thread_terminate(remote_thread));
     kr(vm_deallocate(task, remote_stack, STACK_SIZE));
     kr(vm_deallocate(task, remote_mach_code, MACH_CODE_SIZE));
